12e2e9df502f4ad1dd7e38c8239b3af4786ae4d6
Karlis <karlisstekels.dev@gmail.com>
2023-01-26 08:10:47 +0200

    Initial Commit

---
+++ LearnMVVM/LearnMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnMVVMApp.swift
+//  LearnMVVM
+//
+//  Created by karlis.stekels on 26/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct LearnMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
